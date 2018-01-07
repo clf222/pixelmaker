1be578cf8d2ca2e15ac6e8be6f2a403edff13165
Cindy Francies <clfrancies@comcast.net>
2018-01-07 14:09:08 -0500

    add if statement to remove table if it exists

--- designs.js
+++ designs.js
@@ -2,12 +2,16 @@
 // 1. #pixel_canvas : click, when cell is picked, change color to selected color
 // 2. #sizePicker submit button: set number of rows and column values and create table
 //3. listen for change in color picker, make that the color that number 1 will become.
-
-document.getElementById("make_table").addEventListener("onclick", makeGrid);
+const tblButton = document.getElementById("make_table");
+tblButton.addEventListener('click', makeGrid);
 
 function makeGrid(event) {
-    // do i need a function to check for if tbl exists, document.removeChild(tbl)?
+
     console.log("started");
+    // if table exists, get rid of it
+    if (tbl) {
+        document.body.removeChild(tbl);
+    }
     let rows = document.getElementById("sizePicker").elements["input_height"].value;
     let columns = document.getElementById("input_width").value;
     console.log("Rows: "+rows+ "columns: "+ columns);
