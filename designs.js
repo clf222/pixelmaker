451281392c859145e58ab57f5f2b5219b0fb7290
Cindy Francies <clfrancies@comcast.net>
2018-01-07 13:51:36 -0500

    fixing js and html for table event handler

--- designs.js
+++ designs.js
@@ -2,15 +2,16 @@
 // 1. #pixel_canvas : click, when cell is picked, change color to selected color
 // 2. #sizePicker submit button: set number of rows and column values and create table
 //3. listen for change in color picker, make that the color that number 1 will become.
- 
-document.getElementById("sizePicker").addEventListener("submit", makeGrid);
+
+document.getElementById("make_table").addEventListener("onclick", makeGrid);
 
 function makeGrid(event) {
+    // do i need a function to check for if tbl exists, document.removeChild(tbl)?
     console.log("started");
-    let rows = getElementById("sizePicker").elements["input_height"].value;
-    let columns = getElementById("input_width").value;
+    let rows = document.getElementById("sizePicker").elements["input_height"].value;
+    let columns = document.getElementById("input_width").value;
     console.log("Rows: "+rows+ "columns: "+ columns);
-    const tbl = createElement("table");
+    const tbl = document.createElement("table");
     const tblBody = document.createElement("tbody");
     for (let i=0; i<rows; i++) {
         let row =createElement("tr");
@@ -21,5 +22,5 @@ function makeGrid(event) {
         tblBody.appendChild(row);
     }
     tbl.appendChild(tblBody);
-    body.appendChild(tbl);
+    document.body.appendChild(tbl);
 }
